270ac304492a37a9316a90aa44d712889f2c88b8
Raul Aguilera <jrat9fra@gmail.com>
2019-01-21 15:58:32 -0600

    Initialized repository with simple select example

---
+++ SimpleSelect.sql
@@ -0,0 +1,4 @@
+SELECT --Palabra reservada nos permite indicar que vamos a seleccionar registros
+*      --Vamos a seleccionar todas las columnas
+FROM   --Palabra reservada que precede a la tabla sobre la que vamos a realizar la consulta
+SalesLT.Customer -- Indicamos la tabla sobre la que vamos a hacer la consulta
