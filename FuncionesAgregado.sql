f2c1c485c116891061a9ac12472bcca44e486fab
Raul Aguilera <raul@Rauls-MacBook-Pro.local>
2019-02-27 15:35:29 -0600

    explained funciones de agregado

--- FuncionesAgregado.sql
+++ FuncionesAgregado.sql
@@ -1,11 +1,19 @@
 --Ejemplos funciones de agregado
 
+--AVG
 SELECT ProductCategoryID,AVG(StandardCost)  FROM SalesLT.Product
 GROUP BY ProductCategoryID
 
+--MAX
 SELECT MAX(StandardCost) FROM SalesLT.Product
+SELECT StandardCost FROM SalesLT.Product ORDER BY StandardCost DESC
 
+--MIN
 SELECT MIN(StandardCost) FROM SalesLT.Product
+SELECT StandardCost FROM SalesLT.Product ORDER BY StandardCost ASC
 
+--SUM
 SELECT ProductCategoryID, SUM(StandardCost) FROM SalesLT.Product
 GROUP BY ProductCategoryID
+
+SELECT SUM(StandardCost) FROM SalesLT.Product
